1cc3ad102afa1482d3bc662a345194420f1177ee
Mankor <igorzhelezno@gmail.com>
2023-01-23 21:01:58 +0100

    Add map options WIP

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,9 +4,24 @@ import { Container } from "./styles";
 import { MapSettings } from "./MapSettings";
 import { MapMarkers } from "./MapMarkers";
 import { Artillery, FireMode, ShellType } from "../../utils/types";
+import { useEffect } from "react";
 
 const crs = Leaflet.CRS.Simple;
-const mapExtent = [0.0, -7781.0, 7801.0, 0.0];
+// const mapExtent = [0.0, -7781.0, 7801.0, 0.0]; // cherno
+// const mapOptions = {
+//   zoom: 3,
+//   minZoom: 0,
+//   maxZoom: 6,
+//   tileSize: 128,
+// };
+
+const mapExtent = [0.0, -16384.0, 16384.0, 0.0]; // sahrani3
+const mapOptions = {
+  zoom: 3,
+  minZoom: 0,
+  maxZoom: 6,
+  tileSize: 128,
+};
 
 interface IMap {
   artillery: Artillery;
@@ -15,18 +30,23 @@ interface IMap {
 }
 
 export const Map = ({ artillery, shell, fireMode }: IMap) => {
+  useEffect(() => {
+    fetch("/maps/sahrani3/height_map.txt").then((val) => {
+      console.log("done");
+    });
+  }, []);
   return (
     <Container>
       <MapContainer
         crs={crs}
         center={[0, 0]}
-        zoom={3}
-        minZoom={0}
-        maxZoom={6}
+        zoom={mapOptions.zoom}
+        minZoom={mapOptions.minZoom}
+        maxZoom={mapOptions.maxZoom}
         style={{ height: "100%", width: "100%" }}
         maxBoundsViscosity={1}
       >
-        <TileLayer url="/maps/cherno/{z}/{x}/{y}.png" tileSize={128} noWrap />
+        <TileLayer url="/maps/sahrani3/{z}/{x}/{y}.png" tileSize={mapOptions.tileSize} noWrap />
         <MapMarkers
           crs={crs}
           mapExtent={mapExtent}
