91389d3e2151c21ee0a1dca625f2f002fad0cfbb
Mankor <igorzhelezno@gmail.com>
2024-12-24 18:42:40 +0100

    refactor: Init vite app

--- src/utils/maps/index.ts
+++
@@ -1,7 +0,0 @@
-export { mapCherno } from "./cherno";
-export { mapSahrani } from "./sahrani";
-export { mapAlRayak } from "./al_rayak";
-export { mapVirolahti } from "./virolahti";
-export { mapNapf } from "./napf";
-export { mapAltis } from "./altis";
-export { mapColombia } from "./colombia";
