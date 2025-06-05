32d24882523448b6c1d6a5e217f98a5167b560db
Mankor <igorzhelezno@gmail.com>
2025-06-05 20:19:19 +0200

    fix: Fix typo

--- src/shared/utils/artillery/M5_Sandstorm_MLRS.ts
+++ src/shared/utils/artillery/M5_Sandstorm_MLRS.ts
@@ -37,7 +37,7 @@ export const Arty_M5_Sandstorm_MLRS: Artillery = {
       artilleryCharge: 0.3,
     },
     {
-      name: "6600m-8500",
+      name: "6600m-8500m",
       artilleryCharge: 0.341,
     },
     {
